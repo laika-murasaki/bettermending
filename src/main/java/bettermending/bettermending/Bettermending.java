20fbefc0b7aff6b1b85b6a2bbcc768b0ce3847b0
Laika <111684641+laika-murasaki@users.noreply.github.com>
2023-01-07 02:38:31 +0700

    simplfy code

--- src/main/java/bettermending/bettermending/Bettermending.java
+++ src/main/java/bettermending/bettermending/Bettermending.java
@@ -1,19 +1,16 @@
 package bettermending.bettermending;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-public final class Bettermending extends JavaPlugin implements Listener {
 
-
+class Bettermending extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
@@ -22,37 +19,24 @@ public final class Bettermending extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+
+        if (!player.isSneaking()) {
+            return;
+        }
+
         ItemStack item = player.getInventory().getItemInMainHand();
+        ItemMeta meta = item.getItemMeta();
 
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
-            if (player.isSneaking() && item.getType() != Material.AIR && item.hasItemMeta() && item.getEnchantmentLevel(Enchantment.MENDING) > 0) {
-                int xp = player.getTotalExperience();
+        if (!meta.hasEnchant(Enchantment.MENDING)) {
+            return;
+        }
 
-                // Calculate how much durability the player can repair
-                int repairAmount = xp / 2;
-                int itemDurability = item.getDurability();
-                int newDurability = Math.max(0, itemDurability - repairAmount);
+        int xpCost = 1;
+        int repairAmount = 2;
 
-                // Repair the item and remove the necessary amount of xp
-                item.setDurability((short) newDurability);
-                int xpCost = (itemDurability - newDurability) / 2;
-                int newXp = Math.max(0, xp - xpCost);
-                int newLevel = 0;
-                while (newLevel < player.getLevel() && newXp >= player.getExpToLevel()) {
-                    newXp -= player.getExpToLevel();
-                    newLevel++;
-                }
-                float newProgress = (float) newXp / (float) player.getExpToLevel();
-                player.setExp(newProgress);
-                player.setLevel(newLevel);
-
-
-                // Send a message to the player
-                player.sendMessage(ChatColor.GREEN + "Repaired item for " + (itemDurability - newDurability) + " durability using " + xpCost + " experience points.");
-            }
+        if (player.getTotalExperience() >= xpCost) {
+            player.setTotalExperience(player.getTotalExperience() - xpCost);
+            item.setDurability((short) Math.max(item.getDurability() - repairAmount, 0));
         }
     }
-
-
-
 }
