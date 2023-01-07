32cded4b11787c044dcf49e6d1cf2e27c8d55e63
Laika <111684641+laika-murasaki@users.noreply.github.com>
2023-01-07 21:05:17 +0700

    K

--- src/main/java/bettermending/bettermending/bettermending.java
+++ src/main/java/bettermending/bettermending/bettermending.java
@@ -1,6 +1,9 @@
 package bettermending.bettermending;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,27 +18,24 @@ import java.util.List;
 
 
     public class bettermending extends JavaPlugin implements Listener {
-        public void stats() {
-            // Initialize bStats metrics
-            int pluginId = 17330; // Replace with your plugin id
-            Metrics metrics = new Metrics(this, pluginId);
-        }
     @Override
     public void onEnable() {
+        // Initialize bStats metrics
+        int pluginId = 17330; // Replace with your plugin id
+        Metrics metrics = new Metrics(this, pluginId);
+
         saveDefaultConfig();
         reloadConfig();
 
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
 
-        // Print message to console when plugin is enabled
-        getLogger().info("BetterMending has been enabled!");
-        // Initialize bStats metrics
+        getCommand("bm status").setExecutor(new RepairStatusCommand(this));
+        getCommand("bm reload").setExecutor(new ReloadCommand(this));
+
     }
 
     @Override
     public void onDisable() {
-        // Print message to console when plugin is disabled
-        getLogger().info("BetterMending has been disabled!");
     }
 
         @EventHandler
@@ -76,7 +76,7 @@ import java.util.List;
 
             // Check whether player has bettermending.use permission
             if (!player.hasPermission("bettermending.use")) {
-                player.sendMessage("You do not have permission to use the repair feature!");
+                player.sendMessage(getConfig().getString("noPermissionMessage"));
                 return;
             }
 
@@ -86,4 +86,68 @@ import java.util.List;
                 player.updateInventory();
             }
         }
-}
+        public class RepairStatusCommand implements CommandExecutor {
+            private final JavaPlugin plugin;
+
+            public RepairStatusCommand(JavaPlugin plugin) {
+                this.plugin = plugin;
+            }
+
+            @Override
+            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage("This command can only be run by a player.");
+                    return true;
+                }
+
+                Player player = (Player) sender;
+
+                ItemStack item = player.getInventory().getItemInMainHand();
+                if (item.getDurability() == 0) {
+                    player.sendMessage(getConfig().getString("itemAlreadyRepaired"));
+                    return true;
+                }
+
+                // Check if the item has the MENDING enchantment
+                if (!item.getEnchantments().containsKey(Enchantment.MENDING)) {
+                    player.sendMessage(getConfig().getString("itemDoesNotHaveMending"));
+                    return true;
+                }
+
+                int xpCost = plugin.getConfig().getInt("xpCost");
+                int repairAmount = plugin.getConfig().getInt("repairAmount");
+                int playerExperience = expcu.getPlayerExp(player); // pass player object as argument
+
+                // Calculate how much durability the item has left until it is fully repaired
+                int durabilityLeft = item.getType().getMaxDurability() - item.getDurability();
+                int repairsNeeded = (int) Math.ceil((double) durabilityLeft / repairAmount);
+                int totalXpCost = repairsNeeded * xpCost;
+
+                player.sendMessage(String.format(getConfig().getString("durabilityLeftMessage"), durabilityLeft));
+                player.sendMessage(String.format(getConfig().getString("repairsNeededMessage"), repairsNeeded, totalXpCost));
+
+                return true;
+            }
+        }
+        public class ReloadCommand implements CommandExecutor {
+            private final JavaPlugin plugin;
+
+            public ReloadCommand(JavaPlugin plugin) {
+                this.plugin = plugin;
+            }
+
+            @Override
+            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                if (!sender.hasPermission("bettermending.reload")) {
+                    sender.sendMessage(getConfig().getString("noPermissionMessage"));
+                    return true;
+                }
+
+                plugin.reloadConfig();
+                sender.sendMessage("Config reloaded.");
+                return true;
+            }
+        }
+
+
+    }
