64125b609e49605dd97400dac49cd21026afb306
Laika <111684641+laika-murasaki@users.noreply.github.com>
2023-01-07 19:06:52 +0700

    Add configuration option for allowed enchantments
    Add a configuration option `allowedEnchantments` to allow players to choose which enchantments they want to use with the repair feature. The plugin will only use enchantments from this list for repair.
    
    Modify the event handler to use the `containsKey` method of the `Map` class to check if an `ItemStack` has the MENDING enchantment, and the `contains` method of the `List` class to check if the MENDING enchantment is in the list of allowed enchantments.
    
    Remove the `metrics` variable from the `onEnable` method, as it is never used.
    
    Upgrade to the latest version of the bStats library to fix a compilation error.

--- src/main/java/bettermending/bettermending/bettermending.java
+++ src/main/java/bettermending/bettermending/bettermending.java
@@ -11,12 +11,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bstats.bukkit.Metrics;
+import java.util.List;
 
 
     public class bettermending extends JavaPlugin implements Listener {
-        public void RepairPlugin() {
-            // This is the public constructor of the plugin class
-    }
+        public void stats() {
+            // Initialize bStats metrics
+            int pluginId = 17330; // Replace with your plugin id
+            Metrics metrics = new Metrics(this, pluginId);
+        }
     @Override
     public void onEnable() {
         saveDefaultConfig();
@@ -27,8 +30,6 @@ import org.bstats.bukkit.Metrics;
         // Print message to console when plugin is enabled
         getLogger().info("BetterMending has been enabled!");
         // Initialize bStats metrics
-        int pluginId = 17330; // Replace with your plugin id
-        Metrics metrics = new Metrics(this, pluginId);
     }
 
     @Override
@@ -39,44 +40,50 @@ import org.bstats.bukkit.Metrics;
 
         @EventHandler
         public void onPlayerInteract(PlayerInteractEvent event) {
-
             Action action = event.getAction();
-            // rest of the event handler code
 
-        Player player = event.getPlayer();
+            Player player = event.getPlayer();
 
-        if (!(event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) || !player.isSneaking()) {
-            return;
+            if (!(event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) || !player.isSneaking()) {
+                return;
+            }
+
+            ItemStack item = player.getInventory().getItemInMainHand();
+            ItemMeta meta = item.getItemMeta();
+
+            // Check if the item has the MENDING enchantment
+            if (!item.getEnchantments().containsKey(Enchantment.MENDING)) {
+                return;
+            }
+
+
+            // Check if the MENDING enchantment is in the list of allowed enchantments
+            List<String> allowedEnchantments = getConfig().getStringList("allowedEnchantments");
+            if (!allowedEnchantments.contains(Enchantment.MENDING.getName())) {
+                return;
+            }
+
+            int xpCost = getConfig().getInt("xpCost");
+            int repairAmount = getConfig().getInt("repairAmount");
+
+            int playerExperience = expcu.getPlayerExp(player); // pass player object as argument
+
+
+            if (item.getDurability() == 0) {
+                // Item is already fully repaired, do nothing
+                return;
+            }
+
+            // Check whether player has bettermending.use permission
+            if (!player.hasPermission("bettermending.use")) {
+                player.sendMessage("You do not have permission to use the repair feature!");
+                return;
+            }
+
+            if (playerExperience >= xpCost) {
+                player.giveExp(-xpCost);
+                item.setDurability((short) Math.max(item.getDurability() - repairAmount, 0));
+                player.updateInventory();
+            }
         }
-
-        ItemStack item = player.getInventory().getItemInMainHand();
-        ItemMeta meta = item.getItemMeta();
-
-        if (!meta.hasEnchant(Enchantment.MENDING)) {
-            return;
-        }
-
-        int xpCost = getConfig().getInt("xpCost");
-        int repairAmount = getConfig().getInt("repairAmount");
-
-        int playerExperience = expcu.getPlayerExp(player); // pass player object as argument
-
-
-        if (item.getDurability() == 0) {
-            // Item is already fully repaired, do nothing
-            return;
-        }
-
-        // Check whether player has bettermending.use permission
-        if (!player.hasPermission("bettermending.use")) {
-            player.sendMessage("You do not have permission to use the repair feature!");
-            return;
-        }
-
-        if (playerExperience >= xpCost) {
-            player.giveExp(-xpCost);
-            item.setDurability((short) Math.max(item.getDurability() - repairAmount, 0));
-            player.updateInventory();
-        }
-    }
 }
